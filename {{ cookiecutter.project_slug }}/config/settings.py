7df8dd283f2e1c894eee5cb222ad01ba32a481d8
Michael Karamuth <michael@arsero.com>
2021-12-15 12:26:12 +0400

    refactor: No need to read .env

--- {{ cookiecutter.project_slug }}/config/settings.py
+++ {{ cookiecutter.project_slug }}/config/settings.py
@@ -7,7 +7,6 @@ from django.urls import reverse_lazy
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
