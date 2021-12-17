25f7c70595d7c49efba29869828692e3b385fef7
Michael Karamuth <michael@arsero.com>
2021-12-17 15:02:15 +0400

    refactor: No need for piptools image

--- {{ cookiecutter.project_slug }}/docker/piptools-compile.sh
+++ {{ cookiecutter.project_slug }}/docker/piptools-compile.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-pip-compile --upgrade --generate-hashes --output-file base.txt base.in
-pip-compile --upgrade --generate-hashes --output-file dev.txt dev.in
-pip-compile --upgrade --generate-hashes --output-file prod.txt prod.in
+pip-compile --upgrade --generate-hashes --output-file requirements/base.txt requirements/base.in
+pip-compile --upgrade --generate-hashes --output-file requirements/dev.txt requirements/dev.in
+pip-compile --upgrade --generate-hashes --output-file requirements/prod.txt requirements/prod.in
